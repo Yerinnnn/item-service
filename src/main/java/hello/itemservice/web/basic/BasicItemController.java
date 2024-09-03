a3bb137b4d4dd233bcaca6ca4a9c398aa313a5ad
Yerinnnn <shinyerin0216@gmail.com>
2024-09-04 00:16:19 +0900

    Feat: 상품 등록 폼
    - th:action을 비워놓으면, 현재 url을 그대로 사용함
    - 강조의 의미로, 동일한 url이라면 아예 비워두는 것이 좋음

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -37,6 +38,20 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    // 상품 등록 폼의 URL과 실제 상품 등록을 처리하는 URL을 동일하게 하고,
+    // HTTP 메서드(GET, POST)로 두 기능을 구분
+    // 이렇게 하면 하나의 URL로 등록 폼과, 등록 처리를 깔끔하게 처리 가능
+    // 아주 합리적인 방법
+    @PostMapping("/add")
+    public String save() {
+        return "";
     }
 
     // 테스트용 데이터 추가
